122417ad2bc225a617de7165cb7ca0f89184b975
Abrar Ahmed <ahmeda8@outlook.com>
2013-03-23 15:42:16 -0700

    adding seq delete

--- CatHack2/Assets/Scripts/CloudScript.cs
+++ CatHack2/Assets/Scripts/CloudScript.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CloudScript : MonoBehaviour {
 	
 	public GameObject biscPrefab1;
+	private LinkedList<UnityEngine.Object> ListOfBisc1;
 	// Use this for initialization
 	void Start () {
 		//GameObject friskies =  GameObject.FindGameObjectWithTag("Respawn");
 		//Instantiate(ranaPre,new Vector3(transform.position.x,transform.position.y-0.5f,transform.position.z),Quaternion.identity);
+		ListOfBisc1 = new LinkedList<UnityEngine.Object>() ;
 	}
 	
 	private float Xmov =0.01f;
@@ -35,6 +38,13 @@ public class CloudScript : MonoBehaviour {
 		}
 		
 		if(Input.GetKeyDown(KeyCode.B))
-			Instantiate(biscPrefab1,new Vector3(transform.position.x,transform.position.y-0.5f,transform.position.z),Quaternion.identity);
+		 ListOfBisc1.AddFirst(Instantiate(biscPrefab1,new Vector3(transform.position.x,transform.position.y-0.5f,transform.position.z),Quaternion.identity));
+		
+		if(Input.GetKeyDown(KeyCode.N))
+		{
+			var del = (UnityEngine.Object)ListOfBisc1.Last.Value;
+			Destroy(del);
+			ListOfBisc1.RemoveLast();
+		}
 	}
 }
