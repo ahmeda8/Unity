369b0c0d9cf5bc99e68db2665de88066d2d960da
Abrar Ahmed <ahmeda8@outlook.com>
2013-03-23 17:52:59 -0700

    C

---
+++ CatHack2/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
